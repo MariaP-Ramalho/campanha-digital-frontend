f0f0359ada6db309d0a73df04774ab4bd939eca8
unknown <eduardapramalho@gmail.com>
2025-06-05 16:18:19 -0300

    feat: live report page

---
+++ script/lives.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const liveId = urlParams.get("liveId");
+
+  fetch(`http://localhost:8080/lives`, { credentials: "include" })
+    .then(res => res.json())
+    .then(lives => {
+      const live = lives.find(l => l.liveId === liveId);
+      if (live) {
+        const tag = live.tag?.name || "Sem Tag";
+        const title = live.title || "Sem Título";
+        document.getElementById("liveTitle").textContent = `[${tag}] ${title}`;
+        document.getElementById("liveTag").textContent = `${tag}`;
+      } else {
+        document.getElementById("liveTitle").textContent = "Live não encontrada";
+      }
+    })
+    .catch(err => {
+      console.error("Erro ao buscar live:", err);
+      document.getElementById("liveTitle").textContent = "Erro ao carregar live";
+    });
+
+  fetchComments(liveId);
+}
